9cec736fe0a94d48deec78e6185579a39cdc2954
Sunit121 <sunit.vadhia@gmail.com>
2020-06-08 18:48:03 +0530

    homepage modification done

--- src/app/routingcompenents/home/home.component.ts
+++ src/app/routingcompenents/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit(): void {
+    throw new Error("Method not implemented.");
   }
+  isShow: boolean;
+  topPosToStartShowing = 100;
+  @HostListener('window:scroll')
+  checkScroll() {
+   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
+    console.log('[scroll]', scrollPosition);
+    
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+  // TODO: Cross browsing
+  gotoTop() {
+    window.scroll({ 
+      top: 0, 
+      left: 0, 
+      behavior: 'smooth' 
+    });
+  }
 }
